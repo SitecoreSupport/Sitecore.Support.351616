7d31d6194b299f5bc14a78c7166195496c96f17d
Paul Stupka <paul.stupka@sitecore.com>
2019-08-16 16:37:41 +0300

    project files

---
+++ src/Sitecore.Support.351616/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.351616")]
+[assembly: AssemblyProduct("Sitecore.Support.351616")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Checklist filter is not cleared when the search results are empty")]
+
